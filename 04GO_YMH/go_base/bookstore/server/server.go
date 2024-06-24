9ac45971ec4bce7ea4d45c1dbc76ea0611027a03
yemingheng <1179004618@qq.omc>
2024-06-24 18:53:01 +0800

    http_server

--- 04GO_YMH/go_base/bookstore/server/server.go
+++ 04GO_YMH/go_base/bookstore/server/server.go
@@ -5,20 +5,12 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
+	"time"
 )
 
 type BookStoreServer struct {
 	s   store.Store
 	srv *http.Server
-}
-
-func (bs *BookStoreServer) createBookHandler(w http.ResponseWriter, req *http.Request) {
-	dec := json.NewDecoder(req.Body)
-	var book store.Book
-	if err := dec.Decode(&book); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-
 }
 
 func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
@@ -29,4 +21,56 @@ func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
 	router := mux.NewRouter()
 	router.HandleFunc("/book", srv.createBookHandler)
 	return nil
+}
+
+func (bs *BookStoreServer) createBookHandler(w http.ResponseWriter, req *http.Request) {
+	dec := json.NewDecoder(req.Body)
+	var book store.Book
+	if err := dec.Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	if err := bs.s.Create(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func (bs *BookStoreServer) getBookHandler(w http.ResponseWriter, req *http.Request) {
+	id, ok := mux.Vars(req)["id"]
+	if !ok {
+		http.Error(w, "no id found in request", http.StatusBadRequest)
+		return
+	}
+	book, err := bs.s.Get(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	response(w, book)
+}
+
+func (bs *BookStoreServer) ListenAndServer() (<-chan error, error) {
+	var err error
+	errChan := make(chan error)
+	go func() {
+		err = bs.srv.ListenAndServe()
+		errChan <- err
+	}()
+	select {
+	case err = <-errChan:
+		return nil, err
+	case <-time.After(time.Second):
+		return errChan, nil
+	}
+
+}
+
+func response(w http.ResponseWriter, v interface{}) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
